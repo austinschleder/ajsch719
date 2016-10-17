2ee63d6fc4397bdd03909a7f790febf60f197f87
Austin Schleder <ajsch719@gmail.com>
2016-10-17 16:44:38 -0700

    Flask tutorial

--- app/views.py
+++ app/views.py
@@ -3,4 +3,8 @@ from app import app
 
 @app.route('/')
 def hello():
-	return 'Hello, world!'
+	return 'Hello, World!'
+
+@app.route('/<name>')
+def hello_name(name):
+	return 'Hello, {}!'.format(name)
